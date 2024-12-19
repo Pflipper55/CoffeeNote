5b48a21dbce5e46093ebffbf8d25c875cf788e05
Philipp Leon <phil897414@gmail.com>
2024-12-19 16:39:02 +0100

    Container

--- coffee_note/lib/layout.dart
+++ coffee_note/lib/layout.dart
@@ -1,11 +1,27 @@
-import 'package:flutter/cupertino.dart'
+import 'package:flutter/material.dart';
 
 Widget mainWidget(){
   return Row(
     children: [
-      Image.network('https://picsum.photos/100'),
-      Image.network('https://picsum.photos/100'),
-      Image.network('https://picsum.photos/100'),
+      container(),
+      container()
     ],
+  );
+}
+
+Widget container() {
+  return Expanded(
+    child: Container(
+      decoration: BoxDecoration(
+        color: const Color(0xFFFF9000),
+        border:  Border.all(
+          color: const Color(0xFF000000),
+          width: 5
+          ),
+        borderRadius: BorderRadius.circular(4)
+      ),
+      child: Image.network('https://picsum.photos/100'),
+      margin: const EdgeInsets.all(4),
+    )
   );
 }
