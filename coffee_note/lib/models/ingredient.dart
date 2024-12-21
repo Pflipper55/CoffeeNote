f940ee9b6e6cc0e5d8d676934e9a7edea1d5d727
Philipp Leon <phil897414@gmail.com>
2024-12-21 21:44:09 +0100

    No need for json

--- coffee_note/lib/models/ingredient.dart
+++ coffee_note/lib/models/ingredient.dart
@@ -5,18 +5,4 @@ class Ingredient {
   final String unit;
 
   const Ingredient({required this.id, required this.name, required this.amount, required this.unit});
-
-   Map<String, dynamic> toJson() => {
-      'id': id,
-      'name': name,
-      'amount': amount,
-      'unit': unit,
-    };
-
-  factory Ingredient.fromJson(Map<String, dynamic> json) => Ingredient(
-    id: json['id'],
-    name: json['name'],
-    amount: json['amount'],
-    unit: json['unit'],
-  );
 }
